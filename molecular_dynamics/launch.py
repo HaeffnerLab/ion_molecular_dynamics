471f7ea6836b7438bbb2eed548de7634c87577e1
Hlab, lab-197 <haeffnerlab@gmail.com>
2013-06-20 18:56:22 -0700

    equilibrium position lookup

--- molecular_dynamics/launch.py
+++ molecular_dynamics/launch.py
@@ -5,13 +5,15 @@ python setup.py build_ext --inplace
 import simulation
 import numpy as np
 from simulation_parameters import simulation_parameters as p
+from equilbrium_positions import equilibrium_positions as equil
 import time
 
 starting_positions = np.zeros((p.number_ions, 3))
 
 starting_positions[:, 0] = p.number_ions * [0]
 starting_positions[:, 1] = p.number_ions * [0]
-starting_positions[:, 2] = 10**-6 * np.array([-10, 0, 10])
+starting_positions[:, 2] = equil.get_positions(p.number_ions, p.f_z)
+starting_positions[0, 0] = 10**-5
 start_time = t = time.time()
 output = simulation.simulation(starting_positions, random_seeding = 0)
 finish_time = time.time()
@@ -19,10 +21,8 @@ print 'Simulation took {0:.2f} seconds'.format(finish_time - start_time)
 # print output[0,:]
 
 from matplotlib import pyplot
-time_axis = np.arange(output.shape[1]) * p.timestep * 10**3
+time_axis = np.arange(output.shape[1]) * p.timestep * 10**6
 for i in range(p.number_ions):
-    pyplot.plot(time_axis, output[i, :, 2])
-
-print output[:, -1, 2] * 10**6
+    pyplot.plot(time_axis, output[i, :, 0])
 
 pyplot.show()
--- molecular_dynamics/simulation_parameters.py
+++ molecular_dynamics/simulation_parameters.py
@@ -2,15 +2,15 @@ from __future__ import division
 
 class simulation_parameters(object):
     
-    number_ions = 3
+    number_ions = 5
     #trap frequencies
     f_drve = 30.0 * 10**6#Hz
     f_x = 4.0 * 10**6#MHz
     f_y = 3.0 * 10**6#Hz
     f_z = 0.1 * 10**6#Hz
     #simulation parameters
-    damping = 20000 #optional velocity damping, useful for finding equlibrium positions
-    simulation_duration = 0.002#seconds
+    damping = 0 #optional velocity damping, useful for finding equlibrium positions
+    simulation_duration = 0.0003#seconds
     timestep = (1 / f_drve) /100#seconds
     total_steps = int(simulation_duration / timestep)
     #ion parameters
