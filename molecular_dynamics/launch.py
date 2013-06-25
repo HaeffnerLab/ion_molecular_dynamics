f3010a276e81bd054eef212058c5cc429c47a75c
Hlab, lab-197 <haeffnerlab@gmail.com>
2013-06-24 18:26:49 -0700

    laser ion interaction

--- molecular_dynamics/launch.py
+++ molecular_dynamics/launch.py
@@ -13,25 +13,50 @@ starting_positions = np.zeros((p.number_ions, 3))
 starting_positions[:, 0] = p.number_ions * [0]
 starting_positions[:, 1] = p.number_ions * [0]
 starting_positions[:, 2] = equil.get_positions(p.number_ions, p.f_z)
-starting_positions[0, 0] = 1e-6
+starting_positions[0, 0] = 0
 start_time = t = time.time()
-output = simulation.simulation(starting_positions, random_seeding = 0)
+positions, excitations = simulation.simulation(starting_positions, random_seeding = 15)
+print positions.shape
+velocities = np.diff(positions, axis = 1) / p.timestep
+print velocities.shape
+
+
 finish_time = time.time()
 print 'Simulation took {0:.2f} seconds'.format(finish_time - start_time)
 # print output[0,:]
 
 from matplotlib import pyplot
-time_axis = np.arange(output.shape[1]) * p.timestep * 10**6
+time_axis = np.arange(positions.shape[1]) * p.timestep * 10**6
+energy_x = velocities[0, :, 0]**2 * p.mass * .5 / p.hbar / (2 * np.pi * p.f_x)
 # for i in range(p.number_ions):
 #     pyplot.plot(time_axis, output[i, :, 0])
-# pyplot.plot(time_axis, 10**6 * output[0, :, 0], label = 'left ion')
-pyplot.plot(time_axis, 10**6 * output[-1, :, 0], label = 'right ion')
+# pyplot.plot(time_axis[:-1], velocities[0, :, 0]**2 * p.mass * .5 / p.hbar / (2 * np.pi * p.f_x), label = 'left ion')
+# pyplot.plot(time_axis, 10**6 * positions[-1, :, 0], label = 'right ion')
 # pyplot.plot(time_axis, 10**6 * output[2, :, 0], label = 'center ion')
 pyplot.title(r"5 ions, x - kick propagation, f_z = {} KHz".format(p.f_z / 10.**3), fontsize = 30)
 pyplot.xlabel(r'$\mu s$', fontsize = 30)
 pyplot.ylabel(r'Displacement along x, $\mu m$', fontsize = 30)
 pyplot.tick_params(axis='both', labelsize = 20)
-# pyplot.xlim(0,200)
-pyplot.legend(fontsize = 20)
+# pyplot.xlim(0,2000)
+# pyplot.legend(fontsize = 20)
 
+# pyplot.show()
+print 'mean excitation', excitations.mean(axis = 0)
+
+#downsampling the data to plot
+chunksize = 1000
+numchunks = energy_x.size // chunksize 
+ychunks = energy_x[:chunksize*numchunks].reshape((-1, chunksize))
+xchunks = time_axis[:chunksize*numchunks].reshape((-1, chunksize))
+ 
+# Calculate the max, min, and means of chunksize-element chunks...
+max_env = ychunks.max(axis=1)
+min_env = ychunks.min(axis=1)
+ycenters = ychunks.mean(axis=1)
+xcenters = xchunks.mean(axis=1)
+ 
+# Now plot the bounds and the mean...
+pyplot.fill_between(xcenters, min_env, max_env, color='gray', 
+                 edgecolor='none', alpha=0.5)
+pyplot.plot(xcenters, ycenters)
 pyplot.show()
--- molecular_dynamics/simulation.pyx
+++ molecular_dynamics/simulation.pyx
@@ -1,5 +1,5 @@
 from simulation_parameters import simulation_parameters as p
-from libc.math cimport cos, sqrt
+from libc.math cimport cos, sin, sqrt, acos, M_PI
 import numpy as np
 
 cdef int TOTAL_STEPS = p.total_steps
@@ -12,9 +12,15 @@ cdef double W_Z = p.f_z * 2 * np.pi
 cdef double MASS = p.mass
 cdef double COULOMB_COEFF = p.coulomb_coeff
 cdef double VEL_DAMPING = p.damping
+cdef double HBAR = p.hbar
+#laser parameters
+cdef double GAMMA = p.transition_gamma
+cdef double SATURATION = p.saturation
+cdef double LASER_DETUNING = p.laser_detuning
+cdef double [:] LASER_DIRECTION = p.laser_direction
+cdef double TRANSITION_K_MAG = p.transition_k_mag
 
-
-cdef void calculate_acceleration(double [:, :] position, double [:, :] velocity, double [:, :] current_acceleration, double time, double random_float):
+cdef void calculate_acceleration(double [:, :] position, double [:, :] velocity, double [:, :] current_acceleration, double time, double [:, :] random_floats, char[:] excitation):
     '''
     given the current position, computes the current acceleration and fills in the current_acceleration array
     '''
@@ -22,12 +28,17 @@ cdef void calculate_acceleration(double [:, :] position, double [:, :] velocity,
     cdef int j = 0
     cdef double dx, dy, dz, distance_sq
     cdef double Fx, Fy, Fz
+    cdef double inst_detuning
+    cdef double gamma_laser
+    cdef double p_excited
+    cdef double theta = 0 
+    cdef double phi = 0
     #acceleration due to the trap
     for i in range(NUMBER_IONS):
         current_acceleration[i, 0] = ( (1/2.)*(-W_X**2 + W_Y**2 + W_Z**2) - W_DRIVE * sqrt(W_X**2 + W_Y**2 + W_Z**2) * cos (W_DRIVE * time)) * position[i, 0]
         current_acceleration[i, 1] = ( (1/2.)*( W_X**2 - W_Y**2 + W_Z**2) + W_DRIVE * sqrt(W_X**2 + W_Y**2 + W_Z**2) * cos (W_DRIVE * time)) * position[i, 1]
         current_acceleration[i, 2] = - W_Z**2 *  position[i, 2]
-        #acceleration due to the coulombic repulsion
+    #acceleration due to the coulombic repulsion
     for i in range(NUMBER_IONS):
         for j in range(i + 1, NUMBER_IONS):
             #the double for loop iterations over unique pairs
@@ -51,10 +62,52 @@ cdef void calculate_acceleration(double [:, :] position, double [:, :] velocity,
         current_acceleration[i, 0] += - VEL_DAMPING * velocity[i, 0]
         current_acceleration[i, 1] += - VEL_DAMPING * velocity[i, 1]
         current_acceleration[i, 2] += - VEL_DAMPING * velocity[i, 2]
-        #acceleration due to laser interaction
+    #acceleration due to laser interaction
+    for i in range(NUMBER_IONS):
+        inst_detuning = LASER_DETUNING - TRANSITION_K_MAG * ( LASER_DIRECTION[0] * velocity[i, 0] + LASER_DIRECTION[1] * velocity[i, 1]+ LASER_DIRECTION[2] * velocity[i, 2]) #Delta + k . v
+        gamma_laser = SATURATION /  (1. + (2 * inst_detuning /  GAMMA)**2) * GAMMA / 2.
+        if not excitation[i]:
+            #if atom is not currently excited, calculate probability to get excited
+            p_exc = gamma_laser * TIMESTEP
+            if random_floats[i, 0] <= p_exc:
+                #atom gets excited
+                excitation[i] = 1
+                #acceleration = (momentum change) / ( time * mass)
+                current_acceleration[i, 0] += HBAR * TRANSITION_K_MAG * LASER_DIRECTION[0] / (TIMESTEP * MASS) 
+                current_acceleration[i, 1] += HBAR * TRANSITION_K_MAG * LASER_DIRECTION[1] / (TIMESTEP * MASS) 
+                current_acceleration[i, 2] += HBAR * TRANSITION_K_MAG * LASER_DIRECTION[2] / (TIMESTEP * MASS) 
+            else:
+                #atom stays in the ground state
+                pass
+        else:
+            #if atom is currently excited, it can decay sponteneously or stimulated
+            p_spon = GAMMA * TIMESTEP
+            p_stim = gamma_laser * TIMESTEP
+            if p_spon > 0.1 or p_stim > 0.1:
+                raise Exception ("time step too small to deal with emission")
+            if random_floats[i, 0] <= p_stim:
+                #atom gets de-excited, stimualted
+                excitation[i] = 0
+                current_acceleration[i, 0] -= HBAR * TRANSITION_K_MAG * LASER_DIRECTION[0] / (TIMESTEP * MASS) 
+                current_acceleration[i, 1] -= HBAR * TRANSITION_K_MAG * LASER_DIRECTION[1] / (TIMESTEP * MASS) 
+                current_acceleration[i, 2] -= HBAR * TRANSITION_K_MAG * LASER_DIRECTION[2] / (TIMESTEP * MASS) 
+            elif p_stim < random_floats[i, 0] <= (p_stim + p_spon):
+                #atom gets de-excited, spontaneous, in a random direction
+                excitation[i] = 0
+                theta = 2 * M_PI * random_floats[i, 1]
+                phi = acos(2 * random_floats[i, 2] - 1)
+                current_acceleration[i, 0] += HBAR * TRANSITION_K_MAG * sin(theta) * cos(phi)   / (TIMESTEP * MASS) 
+                current_acceleration[i, 1] += HBAR * TRANSITION_K_MAG * sin(theta) * sin(phi)   / (TIMESTEP * MASS) 
+                current_acceleration[i, 2] += HBAR * TRANSITION_K_MAG * cos(theta)              / (TIMESTEP * MASS) 
+            else:
+                #atom stays excited
+                pass
+                
 
-cdef void do_verlet_integration(double [:, :, :] positions, double [:] random_floats):
+cdef void do_verlet_integration(double [:, :, :] positions, double [:, :, :] random_floats, char [:, :] excitations):
     cdef double[:, :] current_position = positions[:,0,:]
+    initial_excitation = np.zeros(NUMBER_IONS, dtype = np.uint8)
+    cdef char[:] current_excitation = initial_excitation
     initial_acceleration =  np.zeros((NUMBER_IONS, 3))
     cdef double[:, :] current_acceleration = initial_acceleration
     current_velocity_np = np.zeros((NUMBER_IONS, 3))
@@ -70,13 +123,14 @@ cdef void do_verlet_integration(double [:, :, :] positions, double [:] random_fl
             print 'PROGRESS: {} %'.format(next_print_progress)
             next_print_progress = next_print_progress + 10
         current_time = i * TIMESTEP
-        calculate_acceleration(current_position, current_velocity, current_acceleration, current_time, random_floats[i])
+        calculate_acceleration(current_position, current_velocity, current_acceleration, current_time, random_floats[i], current_excitation)
         #cycle over ions
         for j in range(NUMBER_IONS):
             #cycle over coordinates
             for k in range(3):
                 positions[j, i, k] = 2 * positions[j , i - 1, k] -  positions[j, i - 2, k] + current_acceleration[j, k] * TIMESTEP**2
                 current_velocity[j, k] = (positions[j, i, k] - positions[j, i - 2, k]) / (2 * TIMESTEP)
+            excitations[i,j] = current_excitation[j]
         current_position = positions[:,i,:]
     
 def simulation(starting_position, random_seeding = None):
@@ -84,11 +138,13 @@ def simulation(starting_position, random_seeding = None):
     if random_seeding is not None:
         np.random.seed(random_seeding)
     #precalcualte random floats we will be using
-    random_floats = np.random.random(TOTAL_STEPS)
+    random_floats = np.random.random((TOTAL_STEPS, NUMBER_IONS, 3))
     positions = np.zeros((NUMBER_IONS, TOTAL_STEPS, 3))
+    excitations = np.zeros((TOTAL_STEPS, NUMBER_IONS), dtype = np.uint8)
     positions[:, 0, :] = starting_position
     positions[:, 1, :] = starting_position
     cdef double [:, :, :] positions_view = positions
-    cdef double [:] random_float_view = random_floats
-    do_verlet_integration(positions_view, random_float_view)
-    return positions
+    cdef double [:, :, :] random_float_view = random_floats
+    cdef char [:,:] excitations_view = excitations
+    do_verlet_integration(positions_view, random_float_view, excitations_view)
+    return positions, excitations
